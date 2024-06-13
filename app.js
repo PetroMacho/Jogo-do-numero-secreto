947efbe8b91e930477592f3a77454d6343986537
Pietro Mendes <pietro.mendes999@gmail.com>
2024-06-12 21:21:38 -0300

    mudei o enunciado

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function exibirMensagemInicial(){
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jooj do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 exibirMensagemInicial()
